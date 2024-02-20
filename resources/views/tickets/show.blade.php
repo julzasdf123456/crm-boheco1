7bfc67a77b60ba559b9f78084ce695a8f27eebc7
Julio Lopez <julzasdf123456@gmail.com>
2024-02-20 10:18:32 +0800

    updates

--- resources/views/tickets/show.blade.php
+++ resources/views/tickets/show.blade.php
@@ -321,6 +321,7 @@
                     },
                     error : function(error) {
                         // alert(error);
+                        console.log(error)
                     }
                 })
             });
